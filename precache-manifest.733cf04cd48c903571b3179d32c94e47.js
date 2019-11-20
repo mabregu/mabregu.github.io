7b00c685ba150bfc58a6eaba17894e60cf4413a5
N. Martin Abregù <nmabregu@gmail.com>
2019-11-20 11:24:07 -0300

    Updates

---
+++ precache-manifest.733cf04cd48c903571b3179d32c94e47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d1f9428e21c885f1a322eaf3b96c89f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c725f2b66e69737a2f1",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b6a76fe3754f6c75b5b3",
+    "url": "/static/js/2.5e0ee112.chunk.js"
+  },
+  {
+    "revision": "5c725f2b66e69737a2f1",
+    "url": "/static/js/main.ab3268a8.chunk.js"
+  },
+  {
+    "revision": "bfd1521e694d93483f19",
+    "url": "/static/js/runtime-main.14a2f347.js"
+  },
+  {
+    "revision": "511840e3333a1b306d533f4372804f7e",
+    "url": "/static/media/profile.511840e3.jpeg"
+  }
+]);
