a479c61b7e1ef879a443b62a84cc52f8e669c815
N. Martin Abregù <nmabregu@gmail.com>
2019-11-20 12:09:20 -0300

    first portfolio

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import image from '../Assets/profile.jpg';
+import image from '../Assets/profile.jpeg';
 
 class Sidebar extends Component {
   constructor(props) {
